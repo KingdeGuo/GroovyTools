df9d417b8379a8470d64a36bf356fb2af0c74d49
kingdeguo <1263586919@qq.com>
2023-04-01 18:28:43 +0800

    change name

---
+++ tools-groovybasic/src/main/groovy/com/kingdeguo/groovybasic/toolsgroovybasic/learn/Basic.groovy
@@ -0,0 +1,12 @@
+package com.kingdeguo.groovybasic.toolsgroovybasic.learn
+
+class Basic {
+
+    static void main(String[] args) {
+
+        println "Hello World"
+        println "I love Groovy"
+
+    }
+
+}
