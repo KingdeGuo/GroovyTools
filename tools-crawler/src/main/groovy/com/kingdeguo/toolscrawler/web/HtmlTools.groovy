7de5c288d9b2b19bd0abcf4eae1042b45a645e43
kingdeguo <1263586919@qq.com>
2023-04-14 21:37:57 +0800

    add html tool

---
+++ tools-crawler/src/main/groovy/com/kingdeguo/toolscrawler/web/HtmlTools.groovy
@@ -0,0 +1,22 @@
+package com.kingdeguo.toolscrawler.web
+
+import org.jsoup.Jsoup
+
+class WebTools {
+
+    static def getWebRawText(String url) {
+        def text = new URL(url).text
+        return text
+    }
+
+    static def getWebText(String url) {
+        def doc = Jsoup.connect(url).get()
+        def text = doc.text()
+        return text
+    }
+
+    static void main(String[] args) {
+        println getWebText("https://www.baidu.com")
+    }
+
+}
