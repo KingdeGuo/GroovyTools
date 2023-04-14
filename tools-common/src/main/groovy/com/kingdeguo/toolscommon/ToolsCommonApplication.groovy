1f3c552bda9a5f590bbc3a50316b088a9461a470
kingdeguo <1263586919@qq.com>
2023-04-14 22:53:22 +0800

    fix pom

---
+++ tools-common/src/main/groovy/com/kingdeguo/toolscommon/ToolsCommonApplication.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolscommon
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ToolsCommonApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ToolsCommonApplication, args)
+    }
+
+}
---
+++ tools-common/src/test/groovy/com/kingdeguo/toolscommon/ToolsCommonApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolscommon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToolsCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
