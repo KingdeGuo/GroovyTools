1f68942d9fd2b9e6332cb66f171f1fc65293596d
kingdeguo <1263586919@qq.com>
2023-04-14 20:47:26 +0800

    fix blank pom file

---
+++ tools-crawler/src/main/groovy/com/kingdeguo/toolscrawler/ToolsCrawlerApplication.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolscrawler
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ToolsCrawlerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ToolsCrawlerApplication, args)
+    }
+
+}
---
+++ tools-crawler/src/test/groovy/com/kingdeguo/toolscrawler/ToolsCrawlerApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolscrawler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToolsCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
