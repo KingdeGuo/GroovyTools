cb588fd1abe87f9292ad665062d8aaba4fbea6bc
kingdeguo <1263586919@qq.com>
2023-04-14 20:49:23 +0800

    fix blank pom file

---
+++ tools-ai/src/main/groovy/com/kingdeguo/toolsai/ToolsAiApplication.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolsai
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ToolsAiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ToolsAiApplication, args)
+    }
+
+}
---
+++ tools-ai/src/test/groovy/com/kingdeguo/toolsai/ToolsAiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.kingdeguo.toolsai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ToolsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
